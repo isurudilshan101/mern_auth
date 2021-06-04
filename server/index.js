32d6512f3af9fdd6f9130c9c67e47b5a5573675e
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2021-06-04 15:18:13 +0530

    create servers

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
 const express=require("express");
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+dotenv.config();
+
+//set up server
 const app=express();
 
 const PORT=process.env.PORT || 5000;
 
 app.listen(PORT,()=>console.log(`Server started port : ${PORT}`));
 
-app.get('/test',(req,res)=>{
-    res.send("It works!");
+// app.get('/test',(req,res)=>{
+//     res.send("It works!");
+// });
+
+
+//  isuru41
+
+//mongodb+srv://isuru:isuru41@cluster0.nzhft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+//connect to mongodb  
+
+mongoose.connect(process.env.MDB_CONNECT,
+    { useUnifiedTopology: true,
+        useNewUrlParser: true, }, 
+        (err) => {
+    if(err)  return console.error(err);
+  
+   
+    console.log("Connect Successfull");
 });
