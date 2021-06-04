6b60c36323e3544916ce6de92a21b1e614e9124e
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2021-06-04 13:02:46 +0530

    create servers

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express=require("express");
 const app=express();
 
-const PORT=5000;
+const PORT=process.env.PORT || 5000;
 
-app.listen(PORT,()=>console.log(`Server started port on ${PORT}`));
+app.listen(PORT,()=>console.log(`Server started port : ${PORT}`));
+
+app.get('/test',(req,res)=>{
+    res.send("It works!");
+});
