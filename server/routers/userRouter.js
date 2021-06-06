72970c79eaf1cbaa4a4270c84e087d0ca3102917
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2021-06-06 14:05:36 +0530

    login

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -73,4 +73,26 @@ router.post("/", async(req,res)=>{
   
    
 });
+
+
+        //login
+
+router.post("/login",(req,res)=>{
+    try{
+        const {email,password}=req.body;
+
+         //validation
+
+         if(!email || !password )
+         return res
+         .status(400)
+         .json({errorMessage:"Please enter all required fields"});
+         
+
+    }catch(err){
+        console.log(err);
+        res.status(500).send();
+    }
+})
+
 module.exports=router;
